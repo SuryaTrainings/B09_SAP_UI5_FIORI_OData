92524a25062a22cda718f5d90ab0e2a1639c5fe8
SuryaTrainings <info@SuryaTrainings.com>
2021-09-15 10:06:44 +0000

    Third commit

---
+++ template_ui5/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"template_ui5/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
---
+++ view_sample/webapp/index.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    'sap/ui/core/mvc/XMLView'
+], function (XMLView) {
+    'use strict';
+
+    XMLView.create({
+        viewName: "home.view.App"
+    }).then(function (oView) {
+        oView.placeAt("content");
+    })
+
+});
